c74fbb01cd52b9f20e733d3c20444aa8033ea727
YogaDharma21 <yogacode2006@gmail.com>
2024-10-13 14:07:01 +0800

    feat(eloquent): add needed relationship on table

--- backend/app/Models/RecipeAuthor.php
+++ backend/app/Models/RecipeAuthor.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class RecipeAuthor extends Model
 {
     use HasFactory,SoftDeletes;
+
+    public function recipe():HasMany
+    {
+        return $this->hasMany(Recipe::class);
+    }
 }
