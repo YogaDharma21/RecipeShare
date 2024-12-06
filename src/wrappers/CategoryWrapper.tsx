c86ce38c86034b1fa1d3cc038b8d7c23fccb0131
YogaDharma21 <yogacode2006@gmail.com>
2024-12-06 22:47:30 +0800

    feat: mapping categories data

--- src/wrappers/CategoryWrapper.tsx
+++ src/wrappers/CategoryWrapper.tsx
@@ -9,7 +9,7 @@ export default function CategoryWrapper() {
     const [error, setError] = useState<String | null>(null);
 
     useEffect(() => {
-        axios.get('http://backend.test/api/categories')
+        axios.get('http://127.0.0.1:8000/api/categories')
             .then(response => {
                 setCategories(response.data.data);
                 setLoading(false);
@@ -35,18 +35,11 @@ export default function CategoryWrapper() {
                 </div>
                 <div className="swiper w-full mt-3">
                     <Swiper className='w-full mt-3' direction='horizontal' spaceBetween={16} slidesPerView={"auto"} slidesOffsetBefore={20} slidesOffsetAfter={20}>
-                        <SwiperSlide className="!w-fit pb-[30px]">
-                            <CategoryCard />
-                        </SwiperSlide>
-                        <SwiperSlide className="!w-fit pb-[30px]">
-                            <CategoryCard />
-                        </SwiperSlide>
-                        <SwiperSlide className="!w-fit pb-[30px]">
-                            <CategoryCard />
-                        </SwiperSlide>
-                        <SwiperSlide className="!w-fit pb-[30px]">
-                            <CategoryCard />
-                        </SwiperSlide>
+                        {categories.map((category) => (
+                                <SwiperSlide key={category.id} className="!w-fit pb-[30px]">
+                                    <CategoryCard category={category} />
+                                </SwiperSlide>
+                        ))}
                     </Swiper>
                 </div>
             </section>
