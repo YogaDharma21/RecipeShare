a58e64ce9628f12339884042af2387352148b0af
YogaDharma21 <yogacode2006@gmail.com>
2024-12-25 23:26:38 +0800

    feat: link browse page to search page

--- src/pages/Browse.tsx
+++ src/pages/Browse.tsx
@@ -1,7 +1,19 @@
-
-import CategoryWrapper from '../wrappers/CategoryWrapper';
-import BrowseFeaturedRecipesWrapper from '../wrappers/BrowseFeaturedRecipesWrapper';
+import CategoryWrapper from "../wrappers/CategoryWrapper";
+import BrowseFeaturedRecipesWrapper from "../wrappers/BrowseFeaturedRecipesWrapper";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 export default function Browse() {
+    const [searchQuery, setSearchQuery] = useState("");
+    const navigate = useNavigate();
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchQuery(e.target.value);
+    };
+    const handleSearch = () => {
+        if (searchQuery.length > 3) {
+            navigate(`/search?query=${searchQuery}`);
+        }
+    };
     return (
         <>
             <nav className="flex items-center justify-between px-5 mt-[30px]">
@@ -19,10 +31,7 @@ export default function Browse() {
                 </a>
             </nav>
             <div id="SearchForm" className="px-5 mt-[30px]">
-                <form
-                    action="search.html"
-                    className="flex items-center rounded-full p-[5px_14px] pr-[5px] gap-[10px] bg-white shadow-[0_12px_30px_0_#D6D6D652] transition-all duration-300 focus-within:ring-1 focus-within:ring-[#FF4C1C]"
-                >
+                <div className="flex items-center rounded-full p-[5px_14px] pr-[5px] gap-[10px] bg-white shadow-[0_12px_30px_0_#D6D6D652] transition-all duration-300 focus-within:ring-1 focus-within:ring-[#FF4C1C]">
                     <img
                         src="/assets/images/icons/note-favorite.svg"
                         className="w-6 h-6"
@@ -32,16 +41,22 @@ export default function Browse() {
                         type="text"
                         name="search"
                         id="search"
+                        value={searchQuery}
+                        onChange={handleInputChange}
                         className="appearance-none outline-none w-full font-semibold placeholder:font-normal placeholder:text-black"
                         placeholder="Find our best food recipes"
                     />
-                    <button type="submit" className=" flex shrink-0 w-[42px] h-[42px]">
+                    <button
+                        onClick={handleSearch}
+                        type="submit"
+                        className=" flex shrink-0 w-[42px] h-[42px]"
+                    >
                         <img src="/assets/images/icons/search.svg" alt="icon" />
                     </button>
-                </form>
+                </div>
             </div>
             <CategoryWrapper />
-            <BrowseFeaturedRecipesWrapper/>
+            <BrowseFeaturedRecipesWrapper />
             <div
                 id="BottomNav"
                 className="fixed z-50 bottom-0 w-full max-w-[640px] mx-auto border-t border-[#E7E7E7] py-4 px-5 bg-white/70 backdrop-blur"
@@ -90,6 +105,5 @@ export default function Browse() {
                 </div>
             </div>
         </>
-
-    )
+    );
 }
