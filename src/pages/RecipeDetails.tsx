848d3094115f9c1ed4bc1689dbdd4d7f869ba331
YogaDharma21 <yogacode2006@gmail.com>
2025-01-15 07:41:03 +0800

    fix: anchor can't go inside anchor & element after map needed key

--- src/pages/RecipeDetails.tsx
+++ src/pages/RecipeDetails.tsx
@@ -287,7 +287,10 @@ export default function RecipeDetails() {
                             <div className="grid grid-cols-2 gap-5">
                                 {recipe.recipe_ingredients.map(
                                     (recipeIngredient) => (
-                                        <div className="flex flex-col items-center text-center w-full rounded-[20px] p-[14px] gap-[14px] bg-white shadow-[0_12px_30px_0_#D6D6D680]">
+                                        <div
+                                            key={recipeIngredient.id}
+                                            className="flex flex-col items-center text-center w-full rounded-[20px] p-[14px] gap-[14px] bg-white shadow-[0_12px_30px_0_#D6D6D680]"
+                                        >
                                             <div className="thumbnail flex shrink-0 w-full aspect-[138.5/100] rounded-[20px] bg-[#D9D9D9] overflow-hidden">
                                                 <img
                                                     src={`${baseUrl}/${recipeIngredient.ingredient.photo}`}
